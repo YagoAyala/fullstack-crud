6af7532b976abeb991ec6ab54814db61aab7fbc9
YagoAyala <yago.ayala@outlook.com>
2022-11-02 08:18:55 -0300

    feat: created components form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,30 @@
+import GlobalStyle from "./styles/global";
+import styled from "styled-components";
+import Form from "./components/Form.js";
+import { toast, ToasContainer } from "react-toastify";
+import "react-toastify/dist/react-toastify";
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 800px;
+  margin-top 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Title = styled.h2``
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Container>
+        <Title>USUÁRIOS</Title>
+      </Container>
+      <ToasContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />
+      <GlobalStyle />
+    </>
   );
 }
 
