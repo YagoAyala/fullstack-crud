7440325f6ad4783220beb1924b48468da908ac05
YagoAyala <yago.ayala@outlook.com>
2022-11-02 08:36:31 -0300

    feat: create new routes

--- api/routes/users_routes.js
+++ api/routes/users_routes.js
@@ -1,8 +1,14 @@
 import express from "express";
 import { getUsers } from "../constrollers/user_constrollers.js";
 
-const router = express.Router();
+const router = express.Router()
 
 router.get("/", getUsers)
 
-export default router;
+router.post("/", addUser)
+
+router.put("/:id", updateUser)
+
+router.delete("/:id", deleteUser)
+
+export default router
