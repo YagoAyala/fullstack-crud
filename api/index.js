41752dd4b83138c999c6ec5fb926d872dfb14798
YagoAyala <yago.ayala@outlook.com>
2022-11-02 07:53:53 -0300

    feat: created nodejs crud

--- api/index.js
+++ api/index.js
@@ -1,9 +1,12 @@
 import express from "express";
+import routes from "./routes/users_routes.js"
 import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors())
+
+app.use("/", routes)
 
 app.listen(8800);
--- api/routes/users_routes.js
+++ api/routes/users_routes.js
@@ -1,3 +1,8 @@
 import express from "express";
+import { getUsers } from "../constrollers/user_constrollers.js";
 
 const router = express.Router();
+
+router.get("/", getUsers)
+
+export default router;
